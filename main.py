679d09fc43aa5b7b9b0f3488fb5c63ece8b45969
Alexyuhnevich <129598330+Alexyuhnevich@users.noreply.github.com>
2023-04-04 23:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-from telegram.ext import Updater
+import logging
+from aiogram import types
+from aiogram.utils.executor import start_webhook
+from config import bot, dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from db import database, save_message, read_messages, save_chat_mapping, get_sender_id
 
-from handlers import setup_dispatcher
-from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
+YOUR_CHAT_ID = -1001915443747
 
-# Setup bot handlers
-updater = Updater(TELEGRAM_TOKEN)
+async def on_startup(dispatcher):
+    await database.connect()
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
-dp = updater.dispatcher
-dp = setup_dispatcher(dp)
+async def on_shutdown(dispatcher):
+    await database.disconnect()
+    await bot.delete_webhook()
 
-# Run bot
-if HEROKU_APP_NAME is None:  # pooling mode
-    print("Can't detect 'HEROKU_APP_NAME' env. Running bot in pooling mode.")
-    print("Note: this is not a great way to deploy the bot in Heroku.")
+@dp.message_handler()
+async def handle_message(message: types.Message):
+    if message.chat.id == YOUR_CHAT_ID and message.reply_to_message:
+        sender_id = await get_sender_id(message.reply_to_message.message_id)
+        if sender_id:
+            await bot.send_message(chat_id=sender_id, text=message.text)
+    elif message.chat.id != YOUR_CHAT_ID:
+        forwarded_message = await message.forward(chat_id=YOUR_CHAT_ID)
+        await save_chat_mapping(forwarded_message.message_id, message.from_user.id)
 
-    updater.start_polling()
-    updater.idle()
-
-else:  # webhook mode
-    print(f"Running bot in webhook mode. Make sure that this url is correct: https://{HEROKU_APP_NAME}.herokuapp.com/")
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        url_path=TELEGRAM_TOKEN,
-        webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}"
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
     )
-
-    updater.idle()
